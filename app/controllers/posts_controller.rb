9f02346eef12398a543b06e56e16ea0a2a008f83
Andre Voloshin <cddrelive@gmail.com>
2016-09-26 14:30:43 -0700

    Search bar
    implemented super simple search bar

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,16 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   #retrieves all posts from the database
+
   def index
     @posts = Post.all
+      if params[:search]
+    @posts = Post.search(params[:search]).order("created_at DESC")
+  else
+    @posts = Post.all.order("created_at DESC")
   end
+  end
+  
 
   # GET /posts/1
   # GET /posts/1.json
@@ -71,5 +78,6 @@ class PostsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
       params.require(:post).permit(:title, :body)
+
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ApplicationRecord
   validates :title, :presence => true
   has_many :comments
+def self.search(search)
+  # Title is for the above case, the OP incorrectly had 'name'
+  where("title LIKE ?", "%#{search}%")
 end
+end
+
+  
