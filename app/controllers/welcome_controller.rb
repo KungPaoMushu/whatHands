3d8801d104475a0c68c1c4f642e1b5d46960ca4a
Jon Wong <jonlikesapples@gmail.com>
2016-10-15 13:05:53 -0700

    test

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,12 @@
+<<<<<<< Updated upstream
 class WelcomeController < ApplicationController
   def index
   	redirect_to '/index.html'
   end
 end
+=======
+class WelcomeController < ApplicationController
+  def index
+  end
+end
+>>>>>>> Stashed changes
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
@@ -17,3 +18,22 @@ Rails.application.routes.draw do
   #defaults to posts at localhost:3000
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+=======
+Rails.application.routes.draw do
+  get 'welcome/index'
+  root 'welcome#index'
+  resources :posts do
+    resources :comments
+    #Comments are only avaliable inside posts (hence do)
+  end
+
+  get 'login' => 'user_sessions#new'
+  post 'login' => 'user_session#create'
+  delete 'logout' => 'user_sessions#destroy'
+
+  #root 'posts#index'
+  #home page is the post page, no longer needs localhost:3000/posts,
+  #defaults to posts at localhost:3000
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+>>>>>>> Stashed changes
