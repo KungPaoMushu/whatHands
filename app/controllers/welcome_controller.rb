99e429801ad7f99a1f790fa8a1eb02908d51a2da
Voloshiraptor <cddrelive@gmail.com>
2016-11-05 02:27:06 -0700

    Revert "test"
    This reverts commit 3d8801d104475a0c68c1c4f642e1b5d46960ca4a.
    
    # Conflicts:
    #	Gemfile.lock

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,5 @@
-<<<<<<< Updated upstream
 class WelcomeController < ApplicationController
   def index
   	redirect_to '/index.html'
   end
 end
-=======
-class WelcomeController < ApplicationController
-  def index
-  end
-end
->>>>>>> Stashed changes
