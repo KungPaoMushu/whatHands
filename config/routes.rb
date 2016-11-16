118dd5922ecedb4e2a090a523f336d985fe83a5d
Voloshiraptor <andre.voloshin@sjsu.edu>
2016-11-15 17:02:26 -0800

    Added pages controller
    dasl;kdhas

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   resources :recipes
   resources :comments
+  root "pages#show", page: "home"
+  root "pages#show", page: "about"
+  root "pages#show", page: "contact"
+
+  get "/pages/:page" => "pages#show"
+  
   root "recipes#index"
 
   
